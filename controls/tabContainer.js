5f2eb6cf874a60863cb06f48446a8aa8700d17f4
Jijo Joseph <jijojosephk@hotmail.com>
2021-07-11 22:13:44 +0530

    Code refactoring

--- controls/tabContainer.js
+++ controls/tabContainer.js
@@ -1,13 +1,18 @@
 const { Container } = require('./container');
 // eslint-disable-next-line no-unused-vars
 const { List } = require('./list');
-// eslint-disable-next-line no-unused-vars
 const { CreateTabContainerOptions } = require('./createTabContainerOptions');
-// eslint-disable-next-line no-unused-vars
 const { Tab } = require('./tab');
 const { BackButton, ForwardButton } = require('./windowButtons');
 const constants = require('../constants');
+const defaultFunction = () => { };
 
+let _TabContainer_onTabAdd = new WeakMap();
+let _TabContainer_onTabAdded = new WeakMap();
+let _TabContainer_onTabRemove = new WeakMap();
+let _TabContainer_onTabRemoved = new WeakMap();
+let _TabContainer_onTabActivate = new WeakMap();
+let _TabContainer_onTabActivated = new WeakMap();
 class TabContainer extends Container {
 	/**
 	 * @param {CreateTabContainerOptions} options 
@@ -15,6 +20,13 @@ class TabContainer extends Container {
 	constructor(options = {}) {
 		const params = CreateTabContainerOptions.fromJSON(options);
 		super(params);
+		this.onAdd = options.onTabAdd;
+		this.onAdded = options.onTabAdded;
+		this.onRemove = options.onTabRemove;
+		this.onRemoved = options.onTabRemoved;
+		this.onActivate = options.onTabActivate;
+		this.onActivated = options.onTabActivated;
+		
 		for (const tabOption of options.items) {
 			tabOption.position = constants.controls.position.center;
 			let tabButton = new Tab(tabOption);
@@ -43,6 +55,54 @@ class TabContainer extends Container {
 		return super.items;
 	}
 
+	get onTabAdd() {
+		return _TabContainer_onTabAdd.get(this);
+	}
+
+	set onTabAdd(value) {
+		_TabContainer_onTabAdd.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
+	get onTabAdded() {
+		return _TabContainer_onTabAdded.get(this);
+	}
+
+	set onTabAdded(value) {
+		_TabContainer_onTabAdded.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
+	get onTabRemove() {
+		return _TabContainer_onTabRemove.get(this);
+	}
+
+	set onTabRemove(value) {
+		_TabContainer_onTabRemove.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
+	get onTabRemoved() {
+		return _TabContainer_onTabRemoved.get(this);
+	}
+
+	set onTabRemoved(value) {
+		_TabContainer_onTabRemoved.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
+	get onTabActivate() {
+		return _TabContainer_onTabActivate.get(this);
+	}
+
+	set onTabActivate(value) {
+		_TabContainer_onTabActivate.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
+	get onTabActivated() {
+		return _TabContainer_onTabActivated.get(this);
+	}
+
+	set onTabActivated(value) {
+		_TabContainer_onTabActivated.set(this, typeof (value) == constants.types.function ? value : defaultFunction);
+	}
+
 	applyStyles() {
 		super.applyStyles();
 	}
@@ -67,7 +127,6 @@ function tabClick(e, container) {
 		console.log(i, targetTab);
 	});
 }
-
 
 /**
  * @param {HTMLElement} clickTarget 
