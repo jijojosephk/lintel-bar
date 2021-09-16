c185c728dc71c28297d83534a8aa352215aa6664
Jijo Joseph <jijojosephk@hotmail.com>
2021-09-16 22:13:54 +0530

    Code refactoring

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./controls');
+module.exports = {
+	Button: require('./controls/button').Button,
+	Container: require('./controls/container').Container,
+	Control: require('./controls/control').Control,
+	Element: require('./controls/element').Element,
+	Icon: require('./controls/icons').Icon,
+	FontIcon: require('./controls/icons').FontIcon,
+	ImageIcon: require('./controls/icons').ImageIcon,
+	LintelBar: require('./controls/lintelBar').LintelBar,
+	List: require('./controls/list').List,
+	Tab: require('./controls/tab').Tab,
+	TabContainer: require('./controls/tabContainer').TabContainer,
+	MinimizeButton: require('./controls/buttonTypes').MinimizeButton,
+	ResizeButton: require('./controls/buttonTypes').ResizeButton,
+	CloseButton: require('./controls/buttonTypes').CloseButton,
+	AlwaysOnTopToggle: require('./controls/buttonTypes').AlwaysOnTopToggle,
+	WindowControls: require('./controls/windowControls').WindowControls,
+	WindowTitle: require('./controls/windowTitle').WindowTitle
+};
